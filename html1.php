50ff31a15bd13e67b31951ac61ffcb8954497a2d
Krunalia123 <jadhavkrunali622@gmail.com>
2023-05-03 00:11:54 +0530

    first commit

---
+++ html1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Html</title>
+</head>
+<body>
+<h1>This is a heading</h1>
+<p>this is a paragraph.</p>
+<p> this is another paragraph.<p>
+</body>
+</html>
