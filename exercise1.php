df015275ad3a876d79c593826d5f6bc940c59ade
Krunalia123 <jadhavkrunali622@gmail.com>
2023-05-02 23:31:43 +0530

    first commit

---
+++ exercise1.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page title</title>
+
+<style>
+
+.head{
+margin: 0;
+padding: 20px;
+border-colour: black;
+}
+.main{
+border:2px solid black;
+padding:30px;
+margin:2px;
+}
+
+.two {
+  margin: 2px;
+  padding: 5px;
+  border:2px solid black; 
+}
+
+.three{
+  margin: 2px;
+  padding: 5px;
+  border:2px solid black; 
+}
+
+.head{
+margin:2px;
+padding:2px;
+border:2px solid black;
+}
+.html{
+border:2px solid black;
+padding:50px;
+margin:0;
+
+}
+.title{
+margin:2px;
+padding:2px;
+border:2px solid black
+}
+.one{
+border:2px solid black;
+padding:20px;
+margin:2px;
+}
+</style>
+</head>
+<body>
+<article class="html">
+<h3>html</h3>
+	<article class="one">
+	<h3>head</h3>
+		<article class="title">
+		<h3>title(page title)/title</h3>
+		</article>
+	<h3>/head</h3>
+	</article>
+	  	<article class="main">
+		<article class="head">
+		<h1>This is a heading</h1>
+		<b>This is bold</b>
+		<div>
+		<small>smaller text</small></div>
+		<p>my favorite color is <del>red</del><ins>pink</ins></p>
+		</article>
+		<article class="three">
+		<p>This is a <mark>paragraph.</mark></P>
+		</article>
+		<article class="two">
+		<p><em>This is another paragraph.</em></p>
+		</article>
+		</article>
+<h3>/html</h3>
+</article>
+</body>
+</html>
